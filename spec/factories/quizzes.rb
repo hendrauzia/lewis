d21f7252d764b4c8a96eccb8562fe91bd91e1173
Hendra Uzia <hendra.uzia@gmail.com>
2014-06-28 11:43:22 +0700

    Add sample tryout questions

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -1,6 +1,35 @@
 FactoryGirl.define do
   factory :quiz do
     name 'Some Quiz Name'
+    questions Hash[
+      "1" => {
+        "description" => "Some description of the question",
+        "options" => {
+          "a" => "Some answer",
+          "b" => "Another answer",
+          "c" => "Yet another answer"
+        }
+      },
+      "2" => {
+        "description" => "Another description of the question",
+        "options" => {
+          "a" => "Yo, some answer",
+          "b" => "Yo, another answer",
+          "c" => "Yo, yet another answer",
+          "d" => "Yo, here's another answer",
+          "e" => "Yo, here's yet another answer"
+        }
+      },
+      "3" => {
+        "description" => "Yet, another description of the question",
+        "options" => {
+          "a" => "Whoa, some answer",
+          "b" => "Whoa, another answer",
+          "c" => "Whoa, yet another answer",
+          "d" => "Whoa, here's another answer"
+        }
+      }
+    ]
     answers Hash[ "1" => "a", "2" => "c", "3" => "b" ]
     published true
   end
