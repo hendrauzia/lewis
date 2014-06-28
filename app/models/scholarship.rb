3e089f04cb9b477961089a03aecbd81cf92504cf
Hendra Uzia <hendra.uzia@gmail.com>
2014-06-28 22:18:19 +0700

    Add determine scholarship qualification

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -1,4 +1,10 @@
 class Scholarship < ActiveRecord::Base
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true }
   validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
+
+  class << self
+    def determine(value)
+      where(score: 0..value).order(score: :desc).first
+    end
+  end
 end
--- spec/models/scholarship_spec.rb
+++ spec/models/scholarship_spec.rb
@@ -6,4 +6,27 @@ describe Scholarship, type: :model do
 
   it { is_expected.to validate_numericality_of(:score).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100).only_integer }
   it { is_expected.to validate_numericality_of(:discount).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(1) }
+
+  describe ".determine" do
+    before do
+      @stupid  = create(:scholarship, :stupid)
+      @average = create(:scholarship, :average)
+      @smart   = create(:scholarship, :smart)
+      @genius  = create(:scholarship, :genius)
+    end
+
+    context "unqualified" do
+      it "doesn't qualify scholarship" do
+        expect(Scholarship.determine(@average.score - 1).id).to be(@stupid.id)
+      end
+    end
+
+    context "qualified" do
+      it "select the highest qualified scholarship" do
+        expect(Scholarship.determine(@smart.score - 1).id).to be(@average.id)
+        expect(Scholarship.determine(@genius.score - 1).id).to be(@smart.id)
+        expect(Scholarship.determine(@genius.score).id).to be(@genius.id)
+      end
+    end
+  end
 end
