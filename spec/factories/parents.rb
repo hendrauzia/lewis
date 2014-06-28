70a6fea20662c2afa98068d126222f4608ba898e
Hendra Uzia <hendra.uzia@gmail.com>
2014-06-28 07:59:01 +0700

    Add model parent with user inheritance

---
+++ spec/factories/parents.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :parent do
+    email "parent@lol.lol"
+    first_name "Parent"
+    password "passsword"
+  end
+end
---
+++ spec/models/parent_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe Parent, type: :model do
+  it_behaves_like User
+end
