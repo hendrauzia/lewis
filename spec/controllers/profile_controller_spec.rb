7a948afccc06933fa0f14e35b666062f026338df
Hendra Uzia <hendra.uzia@gmail.com>
2014-06-29 22:55:48 +0700

    Fix authenticatable behavior on profile

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe ProfileController, type: :controller do
   it_behaves_like "Authenticatable"
 
-  before do
-    @student = create(:student)
-    session[:user_id] = @student.id
-  end
-
   describe "GET share_profile" do
+    before do
+      @student = create(:student)
+      session[:user_id] = @student.id
+    end
+
     it "returns http success" do
       get 'share'
       expect(response).to be_success
