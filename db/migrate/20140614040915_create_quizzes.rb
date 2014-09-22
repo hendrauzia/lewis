1b34f6da0effe86ce00636c732f2b72aed811f81
Hendra Uzia <hendra.uzia@gmail.com>
2014-09-22 22:20:04 +0700

    Revert "Remove hstore extension on postgresql"
    This reverts commit 7e5518219590a49149ae2851dae3220b6a8d56ab.

--- db/migrate/20140614040915_create_quizzes.rb
+++ db/migrate/20140614040915_create_quizzes.rb
@@ -1,6 +1,7 @@
 class CreateQuizzes < ActiveRecord::Migration
   def change
     create_table :quizzes do |t|
+      enable_extension 'hstore'
       t.string :name
       t.string :description
       t.json :questions
